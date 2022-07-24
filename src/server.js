1b2e9f3d9925e6cedfdf8d0a872c13f6f2339169
israel jeronimo <silvaij16@gmail.com>
2022-07-23 22:53:09 -0300

    corrigindo erros no arquivo dotenv22

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const app = require('./api');
 
-const port = process.env.API_PORT || 3000;
+const port = process.env.PORT || 3000;
 
 // não remova esse endpoint
 app.get('/', (_request, response) => {
